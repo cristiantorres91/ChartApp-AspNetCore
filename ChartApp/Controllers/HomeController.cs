a8f180e537e29d01c53f17d1219ac50608f11f27
cristiantorresalfaro91 <cristiantorresalfaro91@gmail.com>
2021-07-31 11:07:01 -0600

    Agregar archivos de proyecto.

---
+++ ChartApp/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using ChartApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChartApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private List<ProductModel> products;
+
+        public HomeController()
+        {
+            products = new List<ProductModel>
+            {
+                new ProductModel { CodeProduct = "001", Name = "TV", Price = 100,},
+                new ProductModel { CodeProduct = "002", Name = "LAPTOP", Price = 500,},
+                new ProductModel { CodeProduct = "001", Name = "PHONE", Price = 300,},
+
+            };
+        }
+
+        public IActionResult Index()
+        {
+            return View(products);
+        }
+
+        [HttpGet]
+        public JsonResult GenerateChart()
+        {
+
+            return Json(products);
+        }
+
+    }
+}
---
+++ ChartApp/Models/ProductModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChartApp.Models
+{
+    public class ProductModel
+    {
+        public string CodeProduct { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
